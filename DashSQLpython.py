6a0785a0513d5f75eebf9c47e79b91e7eda6cb65
Syed Muhammad Salman Bukhari <116289738+shahg1122@users.noreply.github.com>
2023-09-06 11:46:00 +0500

    Update DashSQLpython.py

--- DashSQLpython.py
+++ DashSQLpython.py
@@ -21,5 +21,57 @@ app.layout = html.Div([
     )
 ])
 
+# Import additional components
+from dash.dependencies import Input, Output
+import dash_table
+
+# Create more figures
+fig2 = px.box(df_product, x='productLine', y='MSRP', title="MSRP by Product Line")
+
+# Initialize the Dash app
+app = dash.Dash()
+
+# Define the layout
+app.layout = html.Div([
+  # Dropdown for selecting metrics
+  html.Label('Select Metric for Bar Plot:'),
+  dcc.Dropdown(
+    id='dropdown-metric',
+    options=[
+      {'label': 'Quantity In Stock', 'value': 'quantityInStock'},
+      {'label': 'MSRP', 'value': 'MSRP'},
+      {'label': 'Buy Price', 'value': 'buyPrice'}
+    ],
+    value='quantityInStock'
+  ),
+
+  # Graphs
+  dcc.Graph(
+    id='bar-plot'
+  ),
+  dcc.Graph(
+    id='box-plot',
+    figure=fig2
+  ),
+
+  # Data Table
+  dash_table.DataTable(
+    id='data-table',
+    columns=[{"name": i, "id": i} for i in df_product.columns],
+    data=df_product.to_dict('records'),
+  )
+])
+
+
+# Define callback to update bar plot
+@app.callback(
+  Output('bar-plot', 'figure'),
+  [Input('dropdown-metric', 'value')]
+)
+def update_bar_chart(selected_metric):
+  return px.bar(df_product, x='productLine', y=selected_metric, title=f"{selected_metric} by Product Line")
+
+
+# Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+  app.run_server(debug=True)
